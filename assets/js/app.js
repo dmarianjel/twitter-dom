38d5958777807c7e5cecb3326f44f1e1c40e1220
dmarianjel <d.a.marianjel@gmail.com>
2017-11-12 16:46:14 -0300

    Listo: contador simple, /n textarea, button.disabled, faltan colores

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,32 +9,44 @@
             var nameSpan = document.createElement("span");
             var dateSpan = document.createElement("span");
             var paragraph = document.createElement("p");
+                //paragraph.classlist.add("color");
             var text = document.createTextNode(texTarea.value);
             paragraph.appendChild(text);
             tuitDiv.appendChild(paragraph);
             tuitDiv.appendChild(nameSpan);
             tuitDiv.appendChild(dateSpan);
-            tuitDiv.setAttribute = ("id", "tuit");
+            tuitDiv.className = "tuit";
             nameSpan.textContent = "Daniela";
             dateSpan.textContent =  new Date();//moment().format('lll')//
+          
             timeLine.insertBefore(tuitDiv, timeLine.children[0]);
+
+        /*}if (texTarea.value == "" && texTarea.value.length >20){
+        btn.disabled= true;}*/
         }
-    };                                   
-var textarea = document.getElementById('comment');
-var heightLimit = 300; /* Maximum height*/ 
-textarea.oninput = function() {
-  textarea.style.height = ""; /* Reset the height*/
-  textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
-};
-/*var maxChar= document.createElement("chartext").value;
-function contChar(e){
-}
-var textarea = document.getElementById('comment');
-textarea.addEventListener('keyup', function (e) {
-    var characters = textarea.value.length;
-    
-});
-var */
+     }   
+    function contwords() {
+    var max = "140";
+    var text = document.getElementById("comment").value;
+    var sendButton= document.getElementById("btn");
+    var longitud= text.length;
+     if(longitud <= max) {
+          document.getElementById("contChar").value = max-longitud;
+          btn.disabled= false;
+     } else if (longitud > max){
+          document.getElementById("comment").value = text.substr(0, max);
+            btn.disabled= true;     
+        }
+        
+     }
+
+    var textarea = document.getElementById("comment");
+    var heightLimit =120; /* Maximum height*/ 
+    textarea.oninput = function() {
+      textarea.style.height = ""; /* Reset the height*/
+      textarea.style.height = Math.min(textarea.scrollHeight, heightLimit) + "px";
+    }
+
 
 
 
